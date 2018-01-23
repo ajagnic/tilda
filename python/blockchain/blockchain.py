2ef6156dd3e6dea812620c2eee31127711b7afd8
Adrian Agnic <adrian@tura.io>
2018-01-23 13:45:41 -0800

    cleanup

--- python/blockchain/blockchain.py
+++ python/blockchain/blockchain.py
@@ -8,14 +8,12 @@ __version__ = '0.0.2'
 __author__ = 'Adrian Agnic'
 
 class Blockchain:
-    """ Defines the blockchain and its methods """
 
     def __init__(self):
         self.__set_difficulty(5)
         self.__genesis()
 
     def __genesis(self):
-        """ Initialize the blockchain with the only valid first block """
         self.chain = [Block(self.__proof_of_work({'data': 'Genesis', 'index':0, 'nonce':0, 'prev_hash':0, 'destination':0, 'origin':0, 'timestamp':time.time()}))]
 
     def __revert_to_valid_block(self):
@@ -23,10 +21,6 @@ class Blockchain:
         self.__genesis()
 
     def __proof_of_work(self, dictionary):
-        """ Calculate a nonce value that results in a Block hash with an amount of leading zeros equal to difficulty
-        :param dictionary: Block properties
-        :type dictionary: dict
-        """
         hashed = Block.sha(dictionary)
         while hashed[0:self.__difficulty[0]] != '0' * self.__difficulty[0]:
             dictionary['nonce'] += 1
@@ -34,12 +28,9 @@ class Blockchain:
         return dictionary
 
     def __set_difficulty(self, num):
-        """ Create immutable property of blockchain POW difficulty
-        """
         self.__difficulty = (num,)
 
     def _save_local(self):# NOTE PICKLE
-        """ Locally store blockchain as file """
         res = os.listdir()
         if '.chaindata' in res:
             doc = open('.chaindata/.data.txt', 'w')
@@ -57,18 +48,9 @@ class Blockchain:
         pass
 
     def get_latest_block(self):
-        """ Returns last Block obj in chain """
         return self.chain[(len(self.chain) - 1)]
 
     def add_new_block(self, data, origin, destination):
-        """ Add new block to chain, calculating hash and verifying
-        :param data: data of new block
-        :type data: dict, str, int, list
-        :param origin: origin of block data
-        :type origin: str
-        :param destination: destination of block data
-        :type destination: str
-        """
         if not isinstance(data, (str, int, list, dict)): return False
         if all(isinstance(i, str) for i in [origin, destination]): return False
         prev_block = self.get_latest_block()
@@ -89,7 +71,6 @@ class Blockchain:
         return res
 
     def validate_chain(self):
-        """ Loop through chain, verifying index, hash, and previous hash values """
         res = self.__validate_gen(self.chain[0]._properties, self.chain[0].get_properties())
         if res is False:
             self.__revert_to_valid_block()
@@ -120,14 +101,12 @@ class Blockchain:
         return True, 'Success'
 
     def __hard_hash_check(self, set_props, prev_set_props):
-        """ validate_chain helper: check immutable hash values """
         if len(set_props) == len(prev_set_props):
             if set_props[3] == prev_set_props[7]:
                 return True
         return False
 
     def __validate_blocks_hash(self, properties):#NOTE TODO REFACTOR FOR BOTH _props & __props
-        """ validate_chain helper: check contents = hash and proof of work used """
         props_copy = copy.deepcopy(properties)
         del props_copy['hash']
         hashed = Block.sha(props_copy)
