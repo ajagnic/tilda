c84aabe38ea031ea617ea089f46bafad2b598b21
Adrian Agnic <adrian@tura.io>
2018-01-23 12:09:19 -0800

    cleanup

--- python/blockchain/block.py
+++ python/blockchain/block.py
@@ -4,13 +4,8 @@ __version__ = '0.0.2'
 __author__ = 'Adrian Agnic'
 
 class Block:
-    """ Defines a block and its methods """
 
     def __init__(self, dictionary):
-        """
-        :param dictionary: only containing fields listed in get_accepted_keys()
-        :type dictionary: dict
-        """
         self.__accepted_keys = ('data', 'index', 'nonce', 'prev_hash', 'destination', 'origin', 'timestamp')
         if isinstance(dictionary, dict): self.__set_properties(dictionary)
 
@@ -21,31 +16,19 @@ class Block:
         if self.verify_dict(dictionary) is False: return None
         self._properties = {key: value for key, value in dictionary.items()}
         self._properties['hash'] = self.sha(self._properties)
-        # store properties as immmutable
         self.__properties = (self._properties['data'], self._properties['index'], self._properties['nonce'], self._properties['prev_hash'], self._properties['destination'], self._properties['origin'], self._properties['timestamp'], self._properties['hash'])
 
     def get_accepted_keys(self):
-        """ Return list of accepted keys to use for initializing a Block """
         return self.__accepted_keys
 
     def get_properties(self):
-        """ Returns tuple of Block instance property values """
         return self.__properties
 
-    def verify_dict(self, dictionary):
-        if isinstance(dictionary, dict):
-            if len(dictionary) == len(self.get_accepted_keys()):
-                for key, value in dictionary.items():
-                    if key in self.get_accepted_keys():
-                        return True
-        return False
+    def verify_dict(self, dictionary):# NOTE REFACTOR
+        pass
 
     @staticmethod
-    def sha(properties):# NOTE ADD VALIDATE TYPES
-        """ Method that accepts fields of a Block, returns hash of all fields
-        :param properties: properties of a Block instance
-        :type properties: dict
-        """
+    def sha(properties):
         cat = "{1}{2}{3}{4}{5}{6}{7}".format(properties['data'], properties['index'], properties['nonce'], properties['prev_hash'], properties['destination'], properties['origin'], properties['timestamp'])
         hashed = hashlib.sha256(cat.encode()).hexdigest()
         return hashed
