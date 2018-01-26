5b0953b26d432718520c8dfef4a5ad3147205d41
Adrian Agnic <adrian@tura.io>
2018-01-25 17:13:23 -0800

    clean slate to actually start api/cli

--- python/server/devel.py
+++ python/server/devel.py
@@ -10,28 +10,29 @@ bfr = Buffer()
 app = Flask(__name__.split('.')[0])
 
 def index():
-    return 'UP'
+    return '~'
 
-def send():
-    origin = request.args.get('origin', '')
-    dest = request.args.get('dest', '')
-    msg = request.args.get('msg', '')
-    print(origin, dest, msg)
-    bfr.list.append([origin, dest, msg])
-    return 'Success', 202
-
-def mine():
-    if (bfr.size() > 0):
-        queue_copy = copy.deepcopy(bfr.list)
-        bfr.list = []
-        return json.dumps(queue_copy), 200
-    else:
-        return 'No Messages', 400
-
+# def send():
+#     origin = request.args.get('origin', '')
+#     dest = request.args.get('dest', '')
+#     msg = request.args.get('msg', '')
+#     print(origin, dest, msg)
+#     bfr.list.append([origin, dest, msg])
+#     return 'Success', 202
+#
+# def mine():
+#     if (bfr.size() > 0):
+#         queue_copy = copy.deepcopy(bfr.list)
+#         bfr.list = []
+#         return json.dumps(queue_copy), 200
+#     else:
+#         return 'No Messages', 400
+#
+#
+# app.add_url_rule('/send', 'send', send, methods=['GET'])
+# app.add_url_rule('/mine', 'mine', mine, methods=['GET'])
 
 app.add_url_rule('/', 'index', index, methods=['GET'])
-app.add_url_rule('/send', 'send', send, methods=['GET'])
-app.add_url_rule('/mine', 'mine', mine, methods=['GET'])
 
 def start():
     app.run()
--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['click', 'requests'],
+    install_requires=['click'],
     entry_points='''
     [console_scripts]
     tilde=cli.tool:tilde
