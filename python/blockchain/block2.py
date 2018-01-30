2910a12192a3c82082ef6d27890aeda2f191a23b
Adrian Agnic <adrian.agnic@hotmail.com>
2018-01-29 19:36:04 -0800

    Update block2.py

--- python/blockchain/block2.py
+++ python/blockchain/block2.py
@@ -2,4 +2,5 @@ __version__ = '0.0.1'
 __author__ = 'Adrian Agnic'
 
 # improvement on Block class, implementing a Block header structure which will allow better indexing from storage
+# simplified implementation, relying on header/hash verification instead of data immutability
 #  NOTE slots?
