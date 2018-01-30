bb00ae5e7623032fc8c96e5cca408e5f22aa45ab
Adrian Agnic <adrian@tura.io>
2018-01-29 19:45:17 -0800

    rm interfaces for now

--- python/blockchain/block2.py
+++ python/blockchain/block2.py
@@ -3,4 +3,4 @@ __author__ = 'Adrian Agnic'
 
 # improvement on Block class, implementing a Block header structure which will allow better indexing from storage
 # simplified implementation, relying on header/hash verification instead of data immutability
-#  NOTE slots?
+#  __dict__ and slots
