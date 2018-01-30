64bdd8bfdadd4f37fc5505b4892fdc2b19027989
Adrian Agnic <adrian@tura.io>
2018-01-29 19:33:56 -0800

    v2 file for block struct

---
+++ python/blockchain/block2.py
@@ -0,0 +1,5 @@
+__version__ = '0.0.1'
+__author__ = 'Adrian Agnic'
+
+# improvement on Block class, implementing a Block header structure which will allow better indexing from storage
+#  NOTE slots?
