017bdc36d35e40c2f496f21202f710bad48f34fc
Adrian Agnic <adrian@tura.io>
2018-01-29 22:54:53 -0800

    pause

--- python/blockchain/block2.py
+++ python/blockchain/block2.py
@@ -8,19 +8,32 @@ class Block:
 
     __slots__ = ['index', 'hash', 'header', '__ps']
 
-    def __init__(self, dictionary):
-        for k,v in dictionary.items():
+    def __init__(self, header_dict):
+        for k,v in header_dict.items():
             self.__setattr__(k,v)
         self.set()
-
-    def __repr__(self):
-        return "Index: {0.index}, \n Hash: {0.hash}, \n Header: {0.header}\n".format(self)
 
     def set(self):
         dct = {'index': self.index, 'hash': self.hash, 'header': self.header}
         self.__ps = list(dct.items())
 
-    def generate_header(self):
+    def pow(self, dictionary, difficulty):
+        hashed = self.sha(dictionary)
+        while hashed[:difficulty] != '0' * difficulty:
+            dictionary['nonce'] += 1
+            hashed = self.sha(dictionary)
+        return dictionary
+
+    def sha(self):
+        cat = "{}".format(self.header)
+        self.hash = hashlib.sha256(cat.encode()).hexdigest()
+
+    @staticmethod
+    def open_sha(plain):
+        return hashlib.sha256(plain.encode()).hexdigest()
+
+    @staticmethod
+    def generate_header(previous_block):
         """
         generate header from given params tbd
         """
@@ -30,23 +43,7 @@ class Block:
             'timestamp': 0,
             'previousHash': 0000000000000000000,
             'difficulty': 0,
-            'nonce': 0
+            'nonce': 0,
+            'body': 0
         }
         return new_header
-
-
-    # def pow(self, dictionary, difficulty):
-    #     """
-    #     Increment a property 'nonce' until the resulting hash has a leading amount of zeros equal to difficulty
-    #     """
-    #     hashed = self.sha(dictionary)
-    #     while hashed[:difficulty] != '0' * difficulty:
-    #         dictionary['nonce'] += 1
-    #         hashed = self.sha(dictionary)
-    #     return dictionary
-    #
-    # @staticmethod
-    # def sha(properties):
-    #     cat = "".format(!)
-    #     hashed = hashlib.sha256(cat.encode()).hexdigest()
-    #     return hashed
