66d9ee8e6965114ed403c54848e80f489c374ace
Adrian Agnic <adrian@tura.io>
2018-01-29 22:27:40 -0800

    start block revision

--- python/blockchain/block2.py
+++ python/blockchain/block2.py
@@ -1,6 +1,52 @@
+import hashlib
+
 __version__ = '0.0.1'
 __author__ = 'Adrian Agnic'
 
-# improvement on Block class, implementing a Block header structure which will allow better indexing from storage
-# simplified implementation, relying on header/hash verification instead of data immutability
-#  __dict__ and slots
+
+class Block:
+
+    __slots__ = ['index', 'hash', 'header', '__ps']
+
+    def __init__(self, dictionary):
+        for k,v in dictionary.items():
+            self.__setattr__(k,v)
+        self.set()
+
+    def __repr__(self):
+        return "Index: {0.index}, \n Hash: {0.hash}, \n Header: {0.header}\n".format(self)
+
+    def set(self):
+        dct = {'index': self.index, 'hash': self.hash, 'header': self.header}
+        self.__ps = list(dct.items())
+
+    def generate_header(self):
+        """
+        generate header from given params tbd
+        """
+        new_header = {
+            'index': 0,
+            'version': __version__,
+            'timestamp': 0,
+            'previousHash': 0000000000000000000,
+            'difficulty': 0,
+            'nonce': 0
+        }
+        return new_header
+
+
+    # def pow(self, dictionary, difficulty):
+    #     """
+    #     Increment a property 'nonce' until the resulting hash has a leading amount of zeros equal to difficulty
+    #     """
+    #     hashed = self.sha(dictionary)
+    #     while hashed[:difficulty] != '0' * difficulty:
+    #         dictionary['nonce'] += 1
+    #         hashed = self.sha(dictionary)
+    #     return dictionary
+    #
+    # @staticmethod
+    # def sha(properties):
+    #     cat = "".format(!)
+    #     hashed = hashlib.sha256(cat.encode()).hexdigest()
+    #     return hashed
