a7743a5964a42a5dce9f729765fc1eaf2b674720
Adrian Agnic <adrian@tura.io>
2018-01-31 22:29:51 -0800

    move over smple tcp

---
+++ python/network/nodes.py
@@ -0,0 +1,52 @@
+import socket
+
+__author__='Adrian Agnic'
+__version__='0.0.1'
+
+
+class Mother:
+    
+    __slots__=['host', 'port', 's', 'ips', 'maxPeers']
+
+    def __init__(self, maxPeers=0):
+        self.host="127.0.0.1"
+        self.port=5999
+        self.s=None
+        self.ips=[]
+        self.maxPeers=maxPeers
+
+    def up(self):
+        self.s=socket.socket()
+        self.s.settimeout(10)
+        self.s.bind((self.host,self.port))
+        self.s.listen(self.maxPeers)
+        s, addr=self.s.accept()
+        self.ips.append(addr)
+        print("New IP: {}".format(str(addr)))
+        self.recv(s)
+
+    def recv(self, s):
+        while True:
+            data=s.recv(1024).decode()
+            if not data:
+                break
+            print(data)
+        s.close()
+
+    def peers(self):
+        return self.ips
+
+    def down():
+        self.s.close()
+
+class Node:
+    def __init__(self):
+        self.remhost='127.0.0.1'
+        self.remport=5999
+
+    def up(self):
+        self.s=socket.socket()
+        self.s.connect((self.remhost,self.remport))
+        msg="Hello World"
+        self.s.send(msg.encode())
+        self.s.close()
