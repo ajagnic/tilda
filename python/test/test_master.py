d1ee067315513d4e7e636f8bf497e0f486487576
Adrian Agnic <adrian@tura.io>
2018-02-20 20:58:24 -0800

    minor

---
+++ python/test/test_master.py
@@ -0,0 +1,14 @@
+import unittest
+from blockchain.master import Tilde
+
+__author__ = "Adrian Agnic"
+__version__ = "0.0.1"
+
+
+class TestMaster(unittest.TestCase):
+    def setUp(self):
+        self.tilde = Tilde("test.db")
+        print(tilde)
+
+if __name__ == '__main__':
+    main()
