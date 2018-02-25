5053482f83bd754ac2872a30893e861f1a0bd73f
Adrian Agnic <adrian@tura.io>
2018-02-25 15:39:34 -0800

    meh

--- python/blockchain/master.py
+++ python/blockchain/master.py
@@ -1,46 +1,60 @@
-import json
+import time
+import hashlib
 
 __version__ = '0.0.1'
 __author__ = 'Adrian Agnic'
 
 
 class Tilde:
+    """ storage interface class """
 
-    __slots__ = ["bc"]
+    def __init__(self, filepath):
+        pass
 
-    def __init__(self, filename, difficulty):
-        self.filecheck(str(filepath))
-        self.bc = Blockchain(int(difficulty))
+    def save(self, data):
+        """ add new block """
+        pass
 
-    def filecheck(self):
-        try:
-            with open(filepath, "r") as doc:
-                chain_data = json.load(doc)
-            doc.close()
-            # init blockchain w/ chain_data
-        except:
-            self.bc.genesis()
-            with open(filepath, "w") as doc:
-                json.dump(self.bc.chain, doc, separators=(",", ":"))
-            doc.close()
-        else:
-            # validate and return
+    def commit(self):
+        """ mine and store """
+        pass
+
 
 
 class Blockchain:
 
-    __slots__ = ["difficulty", "chain"]
+    def __init__(self):
+        pass
 
-    def __init__(self, difficulty):
-        self.difficulty = int(difficulty)
+    def add(self, data):
+        pass
 
-    def genesis(self):# NOTE TODO BLOCK INIT COULD CHANGE
-        self.chain = [Block(self.__proof_of_work({'data': 'Genesis', 'index': 0, 'nonce': 0, 'prev_hash': 0, 'destination': 0, 'origin': 0, 'timestamp': time.time()}))]
 
 
 class Block:
 
-    __slots__ = []
+    def __init__(self, data, prev_block):
+        self.__dict__ = self.create(data, prev_block)
 
-    def __init__(self, data):
+    def create(self, data, prev_block):
+        """
+        :data: any allowed in dict
+        :prev_block: dict
+        :return: dict
+        """
+        prev_index, prev_hash = prev_block["head"]["index"], prev_block["head"]["hash"]
+        block = {
+            "body": {
+                "meta": {
+                    "index": prev_index + 1,
+                    "time": time.time(),
+                    "prevHash": prev_hash,
+                    "nonce": None
+                },
+                "data": data
+            }
+        }
+        return block
+
+    def hash(self, block):
         pass
